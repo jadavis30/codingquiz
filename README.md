901525e260c3585b28d8b4c52262f85240050893
Jared Davis <jadavis@southsidecharter.org>
2020-11-11 01:11:22 -0500

    added high score display, fixed timer bug, fixed answer btns not displaying

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
 # Mountain Bike Quiz
 
 ## Features
-Users will test their knowledge of trail slang.
+Users will test their knowledge of mountain bikes in this timed quiz.
 Quiz will populate one question at a time, record high scores in localStorage, and display them in an array at the end.
+Questions randomize so that the quiz is in a different order.
+Correct answers give time, incorrect answers take time away.
+High scores are submitted with initials and then displayed in an array, with highest values at the top.
+
 
 #### Sources Cited:
 Build Quiz App by Web Dev Simplified--https://www.youtube.com/watch?v=riDzcEQbX6k 
 Quiz Building: https://www.sitepoint.com/simple-javascript-quiz/
 Background image from Lorem Picsum: https://picsum.photos/id/1023/3955/2094
+Pasquale Caiazzo-Help Session
 
-/*Coding quiz will have
- a countdown timer, 
- multiple questions--done
- multiple choice boxes--done 
-hover feature to change color of buttons--done
-needs to state right or wrong at the bottom on next screen
-subtract time when answered wrong
-timer reaches 0 game ends
-moves to next question auto--done(?)
-view high scores link
-start quiz page
-last page allows user to either play again or clear high scores
-needs to tally high score
-allow user to record high score with initials */
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,232 @@
 var startButton = document.getElementById("start-btn");
+var resetButton = document.getElementById("restart-btn");
 var questionContainerElement = document.getElementById("question-container");
 var questionElement = document.getElementById("question");
-var answerButtonsElement = document.getElementById("answers");
+var answersContainer = document.getElementById("answers");
 var shuffledQuestions, currentQuestionIndex;
+var controls = document.getElementById("controls");
+var resetControls = document.getElementById("reset-controls");
+var countElement = document.getElementById("count");
+var submitScoreContainer = document.getElementById("score-submit");
+var highScoreContainer = document.getElementById("high-scores");
+var highScoreListContainer = document.getElementById("high-score-list");
+var userInitials = document.getElementById("user-initials");
+var isPlaying = false;
 var count = 60;
-var highScore = localStorage.getItem("highscore");
+var loserContainer = document.getElementById("loser-container");
+var finalScore = 0;
+var timer;
 
 function startGame() {
+    count = 50;
+    isPlaying = true
+    controls.classList.add("hide");
+    loserContainer.classList.add("hide");
+    submitScoreContainer.classList.add("hide");
+    highScoreContainer.classList.add("hide");
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     nextQuestion()
-    var interval = setInterval(function(){
-        document.getElementById("count").textContent="Time Left: " + count;
-        count--;
-        if (count === -1){
-            clearInterval(interval)
-            alert("You're out of time!")
-
+    timer = setInterval(function(){
+        countElement.textContent="Time Left: " + count;
+        if (isPlaying && count <= 0){
+            alert("You're out of trail!");
+            endGame(false);
         }
+        count--;
     }, 1000); 
     
 }
 
+function endGame(victory) {
+    clearInterval(timer);
+
+    countElement.textContent = "Time Left: " + count;
+    isPlaying = false;
+    restartControls.classList.remove("hide");
+    questionContainerElement.classList.add('hide');
+
+    if(!victory) {
+        loserContainer.classList.remove("hide");
+    }
+    else {
+        submitScoreContainer.classList.remove("hide");
+    }
+}    
+
+function submitScore () {
+    var initials = userInitials.value;
+    
+    if (!initials || initials.trim() === "") { 
+       alert("Please enter your initials");
+       return; 
+    }
+
+    var score = {
+        score: finalScore,
+        user: initials
+    }
+
+    var scoresString = localStorage.getItem("highscore");
+    
+    var scores = [];
+
+    if (scoresString) {
+        scores = JSON.parse(scoreString);
+    }
+
+    scores.push(score);
+    scores.sort(sortScores);
+    
+    highScoreListContainer.innerHTML = "";
+    scores.forEach(score => {
+        var scoreListItem = document.createElement("li");
+        var highScoreName = document.createElement("div");
+        var highScoreValue = document.createElement("div");
+
+        scoreListItem.classList.add("high-score-entry");
+        highScoreName.classList.add("high-score-name");
+        highScoreValue.classList.add("high-score-value");
+
+        highScoreName.innerText = score.user;
+        highScoreValue.innerText = score.score;
+
+        scoreListItem.appendChild(highScoreName);
+        scoreListItem.appendChild(highScoreValue);
+
+        highScoreListContainer.appendChild(scoreListItem);
+
+    });
+
+    localStorage.setItem("highscore", JSON.stringify(scores));
+
+    submitScoreContainer.classList,add("hide");
+    highScoreContainer.classList.remove("hide");
+}
+
+function sortScores(a, b) {
+    return b.score -a.score;
+}
+
 function nextQuestion() {
-    //resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-    questionElement.innerText = question.question
+    resetQuestion();
+
+    questionElement.innerText = question.questionText;
+    answersContainer.dataset.correctAnswer = question.correctAnswer;
     question.answers.forEach(answer => {
-        const button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
+        var answerBtn = document.createElement("button");
+        
+        answerBtn.classList.add("btn");
+        answerBtn.addEventListener("click",selectAnswer);
+        answerBtn.dataset.answerKey = answer.key
+        
+        answerBtn.innerHTML = answer.text;
+        
+        answersContainer.appendChild(answerBtn);
     })
 }
 
+function resetQuestion() {
+    answersContainer.innerHTML = '';
+    questionElement.innerHTML = '';
+}
 
 function selectAnswer(event) {
-    const selectedButton = event.target
-    if (selectedButton === correctAnswer) {
-        count +2;
+    const selectedButton = event.target;
+
+    var answerNode = selectedButton.parentNode;
+
+    var selectAnswerKey = selectedButton.dataset.answerKey;
+    var correctAnswerKey = answerNode.dataset.correctAnswer;
+
+    if (selectAnswerKey === correctAnswerKey) {
+        count += 5;
+        alert('Correct Bro!');
     }
-    else if (selectedButton === !correctAnswer) {
-        count -5;
+    else {
+        count -= 10;
+        alert('Wrong dude!');
     }
+    
     if (shuffledQuestions.length > currentQuestionIndex + 1){
-        nextQuestion()
-    } else {
-        startButton.innerText = "Restart"
-        startButton.classList.remove("hide")
+        currentQuestionIndex ++;
+        nextQuestion();
+    }
+
+    else {
+        finalScore = count;
+        endGame(true);
+       
     }   
 }
 
+
 const questions = [
     {
-        question: "What is a common component of a trail-ready bicycle?",
-        answers: {
-            A:"Drop bars",
-            B:"Aerodynamic tubing",
-            C: "Suspension fork",
-            D:"700x25c"
-        },
+        questionText: "What is a common component of a trail-ready bicycle?",
+        answers: [
+            {
+                key: "A",
+                text:"Drop bars"
+            },
+            { 
+                key: "B",
+                text:"Aerodynamic tubing"
+            },
+            {
+                key:"C",
+                text: "Suspension fork"
+            },
+            {
+                key: "D",
+                text:"700x25c"
+            }
+        ],
         correctAnswer: "C"    
     },
     {
-        question: "Tubeless mountain bike tires require high air pressure to function.",
-        answers: {
-            A: "True",
-            B: "False"
-        },
+        questionText: "Tubeless mountain bike tires require high air pressure to function.",
+        answers: [
+            {
+                key: "A",
+                text: "True"
+            },
+            {
+                key: "B",
+                text: "False"
+            }
+        ],
         correctAnswer: "B"
     },
     {
-        question: "Which of the following are frame materials used in bicycle manufacturing?",
-        answer: {
-            A: "Steel", 
-            B: "Aluminum", 
-            C: "Carbon fiber", 
-            D: "All of the above",
-        },
+        questionText: "Which of the following are frame materials used in bicycle manufacturing?",
+        answer: [
+            {
+                key: "A",
+                text: "Steel"
+            },
+            { 
+                key: "B",
+                text: "Aluminum"
+            },
+            { 
+                key: "C",
+                text: "Carbon fiber"
+            },
+            {
+                key: "D", 
+                text: "All of the above"
+            }
+        ],
         correctAnswer: "D"
     },
     {
-        question: "Smoother tread is preferable for technical, rooty riding.",
+        questionText: "Smoother tread is preferable for technical, rooty riding.",
         answer: {
             A:"True",
             B:"False"
@@ -97,128 +234,25 @@ const questions = [
         correctAnswer: "False"
     },
     {
-        question: "Which of these mountain bike disciplines involves timed sections of downhill riding, with untimed climbs?",
-        answer: {
-            A:"XC", 
-            B:"Downhill", 
-            C:"Enduro",
-            D:"Dual Slalom",
-        },
+        questionText: "Which of these mountain bike disciplines involves timed sections of downhill riding, with untimed climbs?",
+        answer: [
+            {
+                key: "A",
+                text:"XC"
+            },
+            { 
+                key: "B",
+                text:"Downhill" 
+            },
+            {            
+                key: "C",
+                text:"Enduro"
+            },
+            {
+                key: "D",
+                text:"Dual Slalom"
+            }    
+        ],
         correctAnswer: "C"
     }
 ];    
-
-function answerCheck() {
-    var currentQuestionIndex = question.question + 1;
-    var element = document.getElementById("last-answer");
-    element.innerHTML = question.question
-};
-
-function  scoreSubmit() {
-    scoreSubmit.classList.remove("hide")
-    count.textContent
-}
-
-startButton.addEventListener("click", startGame)
-submitButton.addEventListener("click", scoreSubmit)
-
-//function submitScore(){
-    
-
-
-/*
-//global variables for localStorage
-var nameInput = document.querySelector("#name-input");
-var scoreSave = document.querySelector('#high-score-saved');
-var msgDiv = document.querySelector('#msg');
-var userPasswordSpan = document.querySelector('#user-password');
-//timer
-var timerEl = document.getElementById("countdown");
-var mainEl = document.getElementById("main");
-var startBtn = document.getElementById("start");
-
-//message to display at end
-var message =
-    "Nice! Now go ride!";
-var words = message/split (" ");
-
-
-
-
-
-function displayMessage() {
-    var wordCount = 0;
-
-    // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds
-  var msgInterval = setInterval(function() {
-    if (words[wordCount] === undefined) {
-      clearInterval(msgInterval);
-    } else {
-      mainEl.textContent = words[wordCount];
-      wordCount++;
-    }
-  }, 300);
-}
-
-startBtn.onclick = countdown;
-
-
-
-//array of questions
-var questions = [
-    
-    ]
-
-//keeping track of score
-var score = 0;
-
-//iterating through array
-for(let i = 0; i < questions.length; i++) {
-    var userAnswer = confirm(questions[i].question);
-if(userAnswer === questions[i].answer){
-    score++;
-    //alert("Correct!"); isn't the command for here, but we do need to carry this over
-} else {
-    //alert("Wrong!");
-}
-};    
-alert("Finished! Your score was " + score + " out of " + questions.length);
-
-
-//retrieving localStorage
-function renderHighScore() {
-    var name = localStorage.getItem("name-input");
-    var highScore = localStorage.getItem("high-score-saved");
-    if (name === "null" || highScore === "null") {
-        return;
-    }
-.textContent = name;
-.textContent = highScore;    
-};
-
-//display scores
-renderLastScores();
-
-//message to play at the end
-function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute('class', type);
-}
-
-//when hitting submit scores
-savedScores.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    var name = document.querySelector("#name-input").value;
-    var scoreSave =document.querySelector("#high-score-saved").value;
-
-    if(name ==="") {
-        displayMessage("Woah dude! Leave your name for the other shredders, please!");
-    } else {
-        displayMessage("Solid effort, my dude! Nice score!");
-
-        localStorage.setItem("name", name);
-        localStorage.setItem("high-score-saved", highScore);
-        renderLastScores();
-    }
-}); */
